f9dd08e5c54f1802c9bc62300ea16a782791c03e
rasel497 <rasel.csediu44@gmail.com>
2024-03-24 00:04:14 +0600

    website title and url found successfully

--- src/test/java/boeslpackage/boeslClass.java
+++ src/test/java/boeslpackage/boeslClass.java
@@ -20,10 +20,36 @@ public class boeslClass {
         Thread.sleep(2000); // after 3s por password input filed bosbe
         driver.findElement(By.name("password")).sendKeys("Test@2024");
         driver.findElement(By.xpath("/html/body/div/div/div/div/div/form/input[2]")).click();
-// first comment
+
+        Thread.sleep(2000);
+        // navigate to back page //driver.navigate().back();
+        // navigate to refresh or reload page //driver.navigate().refresh();
+        //navigate to forward page //driver.navigate().forward();
+        // navigate to specific //driver.navigate().to("https://boesl.softbd.xyz/profile");
+
+        // check website title
+        String ExpectedTitle= "BOESL - Profile";
+        String ActualTitle= driver.getTitle();
+        if(ActualTitle.equals(ExpectedTitle)){
+            System.out.println("Title match");
+        }
+        else {
+            System.out.println("Title doesn't match!");
+        }
+
+        // Check website url
+        String ExpectedURL= "https://boesl.softbd.xyz/profile";
+        String ActualURL= driver.getCurrentUrl();
+        if(ActualURL.equals(ExpectedURL)){
+            System.out.println("URL Matched");
+        }
+        else {
+            System.out.println("URL doesn't matched!");
+        }
         // second comment
         //ygyu
 //        New change
+//        test new
 
     }
 }
